aa7b93db0aae18338ca60e60b543b0416888b584
Dogukan Arslan <dogukan2arslan@hotmail.com>
2021-03-07 21:22:12 +0300

    Add fontawesome package

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.scss";
 import "toaster-js/default.scss";
+import "@fortawesome/fontawesome-free/css/all.min.css"
 import Home from "./components/Home";
 import Login from "./components/Login";
 
