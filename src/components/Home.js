cdf40ae1b19bedf546771ccb7b637117881d6158
Dogukan Arslan <dogukan2arslan@hotmail.com>
2021-03-07 09:50:58 +0300

    Add login page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Redirect } from "react-router-dom";
 import Main from "./Main";
 import Sidebar from "./Sidebar";
 
 const Home = () => {
   const [cards, setCards] = useState([]);
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    setUser(user);
+  }, []);
+
+  if (user === null) {
+    return <Redirect to="/login" />;
+  }
 
   return (
     <div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,40 @@
+import { useState } from "react";
+import { Redirect } from "react-router-dom";
+
 const Login = () => {
-  return <p>Login</p>;
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [redirected, setRedirected] = useState(false);
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleSurnameChange = (e) => {
+    setSurname(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    localStorage.setItem("user", JSON.stringify({ name, surname }));
+    setRedirected(true);
+  };
+
+  if (redirected) {
+    return <Redirect to="/" />;
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input type="text" value={name} onChange={handleNameChange} />
+        <label htmlFor="name">Surname</label>
+        <input type="text" value={surname} onChange={handleSurnameChange} />
+        <button>Save</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
